b42340bd10bf639395688f98e3bd8aa89c7a3545
Alan Morales <alanrmorales@gmail.com>
2016-09-11 16:31:44 -0500

    Creates a new executable for the renderer

---
+++ renderer_main.cpp
@@ -0,0 +1,7 @@
+#include "renderer_main.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  printf("Hello World\n");
+}
---
+++ renderer_main.h
@@ -0,0 +1,6 @@
+#define RENDERER_MAIN_H
+
+#include <stdio.h>
+
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
