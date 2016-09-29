bea7408a4c4845a2381f8fd9a3e374cc8bbf44a9
Alan Morales <alanrmorales@gmail.com>
2016-09-29 17:42:25 -0500

    Formatted Util

--- Util.cpp
+++ Util.cpp
@@ -1,20 +1,21 @@
 #include "Util.h"
 
-Point lerp(const Point& a, const Point& b, const double delta) { return {(b.x - a.x) * delta + a.x, (b.y - a.y) * delta + a.y};
+Point lerp(const Point &a, const Point &b, const double delta) {
+  return {(b.x - a.x) * delta + a.x, (b.y - a.y) * delta + a.y};
 }
 
-double costFunction(std::vector<Point>& S, std::vector<Point>& T, Point& sPoint, Point& tPoint) {
-    double total_dist = sqrt(sPoint.sqDist(tPoint));
-    unsigned long n = S.size();
-    assert(n == T.size());
-    for (int i = 0; i < n; ++i) {
-        const Point& xS = S[i];
-        const Point& xT = T[i];
-        total_dist += sqrt(sPoint.sqDist(xS)) + sqrt(tPoint.sqDist(xT));
-    }
-    return total_dist;
+double costFunction(std::vector<Point> &S, std::vector<Point> &T, Point &sPoint,
+                    Point &tPoint) {
+  double total_dist = sqrt(sPoint.sqDist(tPoint));
+  unsigned long n = S.size();
+  assert(n == T.size());
+  for (int i = 0; i < n; ++i) {
+    const Point &xS = S[i];
+    const Point &xT = T[i];
+    total_dist += sqrt(sPoint.sqDist(xS)) + sqrt(tPoint.sqDist(xT));
+  }
+  return total_dist;
 }
-
 
 Point Point::operator+(Point &p) { return {x + p.x, y + p.y}; }
 
@@ -22,12 +23,14 @@ Point Point::operator-(Point &p) { return {x - p.x, y - p.y}; }
 
 Point Point::operator*(int k) { return {k * x, k * y}; }
 
-double Point::operator*(Point& p) { return x * p.x + y * p.y; }
+double Point::operator*(Point &p) { return x * p.x + y * p.y; }
 
-Point Point::operator/(double k) { return {x/k, y/k}; }
+Point Point::operator/(double k) { return {x / k, y / k}; }
 
-bool Point::operator==(const Point &other) const { return x == other.x && y == other.y; }
+bool Point::operator==(const Point &other) const {
+  return x == other.x && y == other.y;
+}
 
 std::size_t PointHasher::operator()(const Point &p) const {
-    return std::hash<double>()(p.x) ^ (std::hash<double>()(p.y) << 1);
+  return std::hash<double>()(p.x) ^ (std::hash<double>()(p.y) << 1);
 }
--- Util.h
+++ Util.h
@@ -1,55 +1,52 @@
 #ifndef MINGLEC_POINT_H
 #define MINGLEC_POINT_H
 
-#include <cmath>
-#include <vector>
 #include <cassert>
+#include <cmath>
 #include <functional>
+#include <vector>
 
 static const double PHI = (1 + sqrt(5)) / 2;
 static const int UNGROUPED = -1;
 
-
-#define POINT_ID_NONE    0
-
+#define POINT_ID_NONE 0
 
 typedef uint32_t PointId;
 
 static PointId numPoints = 0;
 
 struct Point {
-    float x = 0, y = 0;
-    PointId id;
+  float x = 0, y = 0;
+  PointId id;
 
-    Point(double x2, double y2) : x((float) x2), y((float) y2), id(++numPoints) {}
-    Point(float x2, float y2) : x(x2), y(y2), id(++numPoints) {}
-    Point() : x(0.0f), y(0.0f), id(POINT_ID_NONE) {}
-    Point operator+ (Point& p);
-    Point operator- (Point& p);
-    Point operator* (int k);
-    Point operator/ (double k);
-    bool operator==(const Point& other) const;
-    double operator* (Point& p);
-    double inline norm() { return sqrt(x * x + y * y); }
-    double inline sqDist(const Point& p) {
-        double dx = x - p.x, dy = y - p.y;
-        return dx * dx + dy * dy;
-    };
+  Point(double x2, double y2) : x((float)x2), y((float)y2), id(++numPoints) {}
+  Point(float x2, float y2) : x(x2), y(y2), id(++numPoints) {}
+  Point() : x(0.0f), y(0.0f), id(POINT_ID_NONE) {}
+  Point operator+(Point &p);
+  Point operator-(Point &p);
+  Point operator*(int k);
+  Point operator/(double k);
+  bool operator==(const Point &other) const;
+  double operator*(Point &p);
+  double inline norm() { return sqrt(x * x + y * y); }
+  double inline sqDist(const Point &p) {
+    double dx = x - p.x, dy = y - p.y;
+    return dx * dx + dy * dy;
+  };
 };
 
 struct PointHasher {
-    std::size_t operator()(const Point& p) const;
+  std::size_t operator()(const Point &p) const;
 };
 
 struct Edge {
-    PointId point1;
-    PointId point2;
-    int weight;
+  PointId point1;
+  PointId point2;
+  int weight;
 };
 
-
 Point lerp(const Point &a, const Point &b, const double delta);
-double costFunction(std::vector<Point>& S, std::vector<Point>& T, Point& sPoint, Point& tPoint);
+double costFunction(std::vector<Point> &S, std::vector<Point> &T, Point &sPoint,
+                    Point &tPoint);
 
-
-#endif //MINGLEC_POINT_H
+#endif // MINGLEC_POINT_H
