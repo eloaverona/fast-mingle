c923e01a38a9de994bc8753ab3fded3f5debf287
Alan Morales <alanrmorales@gmail.com>
2017-04-07 10:01:38 -0500

    Using boost.program_options to take the command line arguments of mingle.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,69 @@
-using namespace std;
 #include "main.h"
 #include "Graph.h"
+#include "boost/program_options.hpp"
+#include <iostream>
+#include <string>
+
+const size_t ERROR_IN_COMMAND_LINE = 1;
+const size_t SUCCESS = 0;
+const size_t ERROR_UNHANDLED_EXCEPTION = 2;
 
 int main(int argc, char *argv[]) {
-  if (argc != 5) {
-    fprintf(stderr, "usage: %s path_input_vertices.txt path_input_edges.txt "
-                    "path_output_vertices.txt "
-                    "path_output_edges.txt\n",
-            *argv);
-    return 1;
+  try {
+    boost::program_options::options_description desc("Options");
+    desc.add_options()("help", "Print help message")(
+        "input_vertices",
+        boost::program_options::value<std::string>()->required(),
+        "A file containing the list of input vertices in the original graph. "
+        "See the test_data directory for example of the format.")(
+        "input_edges", boost::program_options::value<std::string>()->required(),
+        "A file containing the list of input edges in the original graph. "
+        "These should be pairs of vertex ids. The ids need to exist in the "
+        "vertices file. See the test_data directory for example of the "
+        "format.")(
+        "output_vertices",
+        boost::program_options::value<std::string>()->required(),
+        "The output vertices that were created in the mingling process.")(
+        "output_edges",
+        boost::program_options::value<std::string>()->required(),
+        "The output edges of the final, mingled graph.");
+
+    boost::program_options::variables_map variables_map;
+    try {
+      boost::program_options::store(
+          boost::program_options::parse_command_line(argc, argv, desc),
+          variables_map);
+
+      if (variables_map.count("help")) {
+        std::cout << "MINGLE implements the mingle edge bundling algorithm "
+                     "that bundles edges into graphs recursively. The "
+                     "algorithm is detailed on this paper: "
+                     "http://yifanhu.net/PUB/edge_bundling.pdf."
+                  << std::endl
+                  << desc << std::endl;
+        return SUCCESS;
+      }
+
+      boost::program_options::notify(variables_map);
+    } catch (boost::program_options::error &e) {
+      std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+      std::cerr << desc << std::endl;
+      return ERROR_IN_COMMAND_LINE;
+    }
+    Graph *graph = new Graph();
+    printf("Created graph.\n");
+    graph->readVerticesAndEdges(
+        variables_map["input_vertices"].as<std::string>().c_str(),
+        variables_map["input_edges"].as<std::string>().c_str());
+    graph->doRecursiveMingle();
+    graph->writePointsAndEdges(
+        variables_map["output_vertices"].as<std::string>().c_str(),
+        variables_map["output_edges"].as<std::string>().c_str());
+    return SUCCESS;
+  } catch (std::exception &e) {
+    std::cerr << "Unhandled Exception reached the top of main: " << e.what()
+              << ", application will now exit" << std::endl;
+    return ERROR_UNHANDLED_EXCEPTION;
   }
-  Graph *graph = new Graph();
-  printf("Created graph.\n");
-  graph->readVerticesAndEdges(argv[1], argv[2]);
-  graph->doRecursiveMingle();
-  graph->writePointsAndEdges(argv[3], argv[4]);
-  return 0;
+  return SUCCESS;
 }
