bb3a2e93bf2aa84a67d20e341f95187a702fb8ea
Alan Morales <alanrmorales@gmail.com>
2017-04-23 15:39:35 -0500

    New module try

--- mingle_pymodule/_mingle.c
+++ mingle_pymodule/_mingle.c
@@ -1,5 +1,7 @@
 #include <Python.h>
 #include <stdio.h>
+#include "Graph.h"
+#include "Vertex.h"
 
 /* Docstrings */
 static char module_docstring[] =
@@ -33,71 +35,27 @@ static PyObject *mingle_load_vertices(PyObject *self, PyObject *args)
   char *verticesFilePath = PyString_AsString(verticesFilePathObject);
 
   std::ifstream verticesStream(verticesFilePath);
+  if (!verticesStream.is_open()) {
+    return PyErr_SetString(PyExc_RuntimeError, "Could not open vertices file. Check file path.");
+  }
   int numPoints;
   verticesStream >> numPoints;
   for (int i = 0; i < numPoints; i++) {
     readNextPointInFile(verticesStream);
   }
   verticesStream.close();
+  Py_RETURN_TRUE;
 }
 
-
-
-static PyObject *load_graph(PyObject *self, PyObject *args)
-{
-	PyStringObject *nodesFilePathObject;
-	PyStringObject *edgesFilePathObject;
-	if (!PyArg_ParseTuple(args, "ss", nodesFilePath, edgesFilePath)){
-      return NULL;
+static readNextPointInFile(std::ifstream &verticesStream) {
+  float xCoord;
+  float yCoord;
+  std::string pointIdString;
+  try {
+    verticesStream >> pointIdString >> xCoord >> yCoord;
+  } catch (...) {
+    return PyErr_SetString(PyExc_RuntimeError, "Vertices file has unexpected format.");
   }
-  char *nodesFilePath = PyString_AsString(nodesFilePathObject);
-  char *edgesFilePath = PyString_AsString(edgesFilePathObject);
-
-  FILE *nodesFilePointer = fopen(nodesFilePath);
-  assert(nodesFilePointer != nullptr);
-  int nodeId;
-  double xCoord;
-  double yCoord;
-  while(fscanf(nodesFilePointer, "%d", &nodeId) != EOF &&
-  	  fscanf(nodesFilePointer, "%f", &xCoord) != EOF &&
-  	  fscanf(nodesFilePointer, "%f", &yCoord) != EOF) {
-
-  }
-  fclose(nodesFilePointer);
-
-	FILE *edgesFilePath = fopen(edgesFilePath);
-	assert(edgesFilePointer != nullptr);
-	int nodeId1;
-	int nodeId2;
-	int weight;
-	while(fscanf(edgesFilePointer, "%d", &nodeId1) != EOF &&
-		  fscanf(edgesFilePointer, "%d", &nodeId2) != EOF &&
-		  fscanf(edgesFilePointer, "%d", &weight)) {
-	}
-    fclose(edgesFilePointer);
-
-    fscanf(nodesFilePointer, "%d");
-}
-
-static PyObject *get_points_in_square(PyObject *self, PyObject *args)
-{
-  double x_lower, x_upper, y_lower, y_upper;
-  /* Parse the input arguments */
-  if (!PyArg_ParseTuple(args, "dddd", &x_lower, &x_upper, &y_lower, &y_upper)){
-    return NULL;
-  }
-  points[] = [];
-  int points_list_length = 1;
-  PyObject *points_list = PyList_New(points_list_length)
-  if(!points_list){
-  	return NULL;
-  }
-  for(i = 0; i < points_list_length; i++) {
-  	PyObject *point = Py_BuildValue("dd", points_list_length[i][0], points_list_length[i][1]);
-  	if(!point) {
-  		Py_DECREF(points_list);
-  		return NULL;
-  	}
-  	PyList_SET_ITEM(points_list, i, point);
-  }
+  Vertex vertex = {xCoord, yCoord};
+  add_vertex(pointIdString, vertex);
 }
