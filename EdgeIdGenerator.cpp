4ac4f830e1f1073b85577bc39822c303a9925d4d
Eloa Franca Verona <efrancav@macalester.edu>
2017-06-15 10:59:20 -0500

    added id generator files

---
+++ EdgeIdGenerator.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Sen on 6/14/17.
+//
+
+#include "EdgeIdGenerator.h"
+
+unsigned long EdgeIdGenerator::lastId = 0;
+
+EdgeIdGenerator::EdgeIdGenerator() {
+}
---
+++ EdgeIdGenerator.h
@@ -0,0 +1,39 @@
+//
+// Created by Sen on 6/14/17.
+//
+
+#ifndef MINGLE_EDGEIDGENERATOR_H
+#define MINGLE_EDGEIDGENERATOR_H
+
+
+#include <printf.h>
+#include <stdlib.h>
+
+class EdgeIdGenerator {
+private:
+    static unsigned long lastId;
+
+public:
+    /**
+   * Build new EdgeIdGenerator
+   */
+    EdgeIdGenerator();
+
+    EdgeIdGenerator(long lastId);
+
+
+
+    char *generateNewID(){
+        unsigned long newIDnumber = lastId + 1;
+
+        lastId = newIDnumber;
+        char *id;
+        id = (char *) malloc(sizeof(char) + sizeof(long));
+        snprintf(id, sizeof(char) + sizeof(long), "e%lu", newIDnumber);
+
+        return id;
+    }
+};
+
+
+#endif //MINGLE_EDGEIDGENERATOR_H
